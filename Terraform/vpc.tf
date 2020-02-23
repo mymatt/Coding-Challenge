5595960ff00f9561471c8098aa5b928733178772
Matthew Myers <mym3491@yahoo.com.au>
2020-02-24 03:30:40 +1100

    added VPC definition

---
+++ Terraform/vpc.tf
@@ -0,0 +1,14 @@
+#---------------------------------------------------
+# Define VPC
+#---------------------------------------------------
+
+resource "aws_vpc" "default" {
+  cidr_block = var.vpc_cidr
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "tf-vpc"
+  }
+}
