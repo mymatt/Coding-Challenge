3223e1fb3763f17e4874a30adeab9af874286e75
Matthew Myers <mym3491@yahoo.com.au>
2020-02-24 03:39:54 +1100

    web server security group

--- Terraform/security.tf
+++ Terraform/security.tf
@@ -88,3 +88,44 @@ resource "aws_security_group" "elb_web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+#---------------------------------------------------
+# Security Group - Web Server
+#---------------------------------------------------
+
+resource "aws_security_group" "web_sg" {
+  name = "web_sg"
+
+  depends_on = [aws_security_group.elb_web_sg]
+
+  vpc_id = aws_vpc.default.id
+
+  ingress {
+    from_port       = "22"
+    to_port         = "22"
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg_pub.id]
+  }
+
+  ingress {
+    from_port = "80"
+    to_port   = "80"
+    protocol  = "tcp"
+    # cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.elb_web_sg.id, aws_security_group.bastion_sg_pub.id]
+  }
+
+  ingress {
+    from_port       = "8"
+    to_port         = "0"
+    protocol        = "icmp"
+    security_groups = [aws_security_group.bastion_sg_pub.id]
+  }
+
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
