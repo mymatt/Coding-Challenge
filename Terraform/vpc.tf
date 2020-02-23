37016113e9ab1ce0c7eeddffd5191eb4eacd7254
Matthew Myers <mym3491@yahoo.com.au>
2020-02-24 03:31:27 +1100

    public subnetss defined and Internet gateway

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -12,3 +12,69 @@ resource "aws_vpc" "default" {
     Name = "tf-vpc"
   }
 }
+
+#---------------------------------------------------
+# Setup Public Subnets - for ALB
+#---------------------------------------------------
+
+resource "aws_subnet" "public-subnet" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.public_subnet_cidr
+  availability_zone = var.availability_zone_1
+
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "tf-public-subnet"
+  }
+}
+
+# Backup for ALB
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.public_subnet_2_cidr
+  availability_zone = var.availability_zone_2
+
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "tf-public-subnet-2"
+  }
+}
+
+#---------------------------------------------------
+# Internet Gateway - Public Subnet
+#---------------------------------------------------
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.default.id
+
+  tags = {
+    Name = "tf-gateway"
+  }
+}
+
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.default.id
+
+  tags = {
+    Name = "tf-pub-route-table"
+  }
+}
+
+resource "aws_route" "pub_int_gateway" {
+  route_table_id         = aws_route_table.public-rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# assign route table to public subnet
+resource "aws_route_table_association" "rt-as-pub-1" {
+  subnet_id      = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.public-rt.id
+}
+
+resource "aws_route_table_association" "rt-as-pub-2" {
+  subnet_id      = aws_subnet.public-subnet-2.id
+  route_table_id = aws_route_table.public-rt.id
+}
